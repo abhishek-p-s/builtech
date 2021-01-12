26e1792e65ae461a29b5bc42ffab33b6b4581411
abhishek <abhishekps6282@gmail.com>
2021-01-12 13:33:55 +0530

    demo page added

--- management.php
+++ management.php
@@ -62,37 +62,68 @@
             </script> BUILTECH</p>
     </aside>
 </div>
-<!-- second page -->
 <div id="bt-main">
-    <h1>Our Management</h1>
-    <div class="row p-5">
-        <div class="col-7">
-            <p class="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
-                Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
-                Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
-                Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
-
+    <aside id="bt-hero" class="js-fullheight">
+        <div class="flexslider js-fullheight">
+            <ul class="slides">
+                <li class="ab_bt-hp-banner-1" style="background-image: url(images/projects/Rectangle.png );">
+                    <div class="overlay"></div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12 col-xs-12 js-fullheight slider-text">
+                                <!-- col-md-offset-3 col-md-push-3 -->
+                                <div class="slider-text-inner">
+                                    <div class="ab_desc">
+                                        <div class="ab_desc-inner top">
+                                            <h2>About Us</h2>
+                                            <p class="ab_pr-intro">Being the pioneer of the apartment concept in Palakkad, Builtech stands tall on the foundation of its values - delivering quality, redefining customer lifestyles, cost effectiveness, time bound delivery ensuring customer
+                                                satisfaction and a proven track-record of post sales service.
+                                                It’s philosophy is founded on transparent dealings and maintaining ever lasting relationships with customers. Builtech’s credibility and good will has been meticulously developed over 35 years of integrity and highly satisfied customers.</p>
+                                            <a href="about.php" class="btn btn-primary btn-learn">Contact Us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </div>
-        <div class="col-5">
-            <img class="img1" src="images/image second.png" alt="image">
-            <span class="tab-head1">Mr.Rajan<br>Directer?</span>
-        </div>
-    </div>
-
-    <div class="row p-5">
-
-        <div class="col-5">
-            <img class="img2" src="images/second image 2.jpg" alt="image"><br>
-            <span class="tab-head2">Ar. Nithin Rajan
-                Director?</span>
-        </div>
-
-        <div class="col-7">
-            <p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
-                Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
-                Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
-                Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
-        </div>
-    </div>
 </div>
-<?php include 'scripts.php'; ?>
+<aside>
+    </div>
+    <!-- second page -->
+    <div id="bt-main">
+        <h1>Our Management</h1>
+        <div class="container">
+            <div class="row p-5">
+                <div class="col-md-7">
+                    <p class="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
+                        Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
+                        Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
+                        Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
+
+                </div>
+                <div class="col-md-5">
+                    <img class="img1" src="images/image second.png" alt="image">
+                    <!-- <span class="tab-head1">Mr.Rajan<br>Directer?</span> -->
+                </div>
+            </div>
+
+            <div class="row p-5">
+                <div class="col-md-5">
+                    <img class="img2" src="images/second image 2.jpg" alt="image"><br>
+                    <!-- <span class="tab-head2">Ar. Nithin Rajan
+                Director?</span> -->
+                </div>
+
+                <div class="col-md-7">
+                    <p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
+                        Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
+                        Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
+                        Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'scripts.php'; ?>
