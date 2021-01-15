d4d2acabb3f067ecf73d8422c5942a5c776edf03
abhishek <abhishekps6282@gmail.com>
2021-01-15 13:15:02 +0530

    image changed to png

--- about.php
+++ about.php
@@ -4,124 +4,143 @@
 
 <!-- scroll to top  -->
 <div id="scroll-to-top" style="display:none;">
-	<div class="img-wrap">
-		<img src="images/icons/up-arrow.png" alt="scroll to top">
-	</div>
+    <div class="img-wrap">
+        <img src="images/icons/up-arrow.png" alt="scroll to top">
+    </div>
 </div>
 
 <div id="bt-page">
-	<!-- desktop nav  -->
-	<aside id="bt-aside" role="complementary" class="border js-fullheight">
-		<a href="index.php" id="bt-logo">
-			<span class="logo-wrap">
-				<img src="images/logo.png" alt="Builtech Logo">
-			</span>
-		</a>
-		<nav id="bt-main-menu" role="navigation">
-			<ul>
-				<li><a href="#bt-about-section">About Us</a></li>
-				<li><a href="#bt-projects-section">Projects</a></li>
-				<li><a href="#bt-contact-section">Builtech edge</a></li>
-				<li><a href="#bt-contact-section">Home Owners</a></li>
-				<li><a href="#bt-contact-section">Contact Us</a></li>
-			</ul>
-		</nav>
+    <!-- desktop nav  -->
+    <aside id="bt-aside" role="complementary" class="border js-fullheight">
+        <a href="index.php" id="bt-logo">
+            <span class="logo-wrap">
+                <img src="images/logo.png" alt="Builtech Logo">
+            </span>
+        </a>
+        <nav id="bt-main-menu" role="navigation">
+            <ul>
+                <li><a href="#bt-about-section">About Us</a></li>
+                <li><a href="#bt-projects-section">Projects</a></li>
+                <li><a href="#bt-contact-section">Builtech edge</a></li>
+                <li><a href="#bt-contact-section">Home Owners</a></li>
+                <li><a href="#bt-contact-section">Contact Us</a></li>
+            </ul>
+        </nav>
 
-		<p class="copyrights">All Rights Reserved | &copy; <script>
-				document.write(new Date().getFullYear());
-			</script> BUILTECH</p>
-	</aside>
+        <p class="copyrights">All Rights Reserved | &copy; <script>
+                document.write(new Date().getFullYear());
+            </script> BUILTECH</p>
+    </aside>
 
-	<!-- tab and smartphone nav  -->
-	<aside id="bt_sm_navbar1" class="bt_sm_navbar">
-		<div id="bt_btn1" class="bt_btn">
-			<div class="bt_line"></div>
-			<div class="bt_line"></div>
-			<div class="bt_line"></div>
-		</div>
-		<nav class="bt_sidebar">
-			<div class="bt_sm-logo-wrap">
-				<a href="index.php" id="bt_sm_logo">
-					<span class="bt_logo-wrap">
-						<img src="images/logo.png" alt="Builtech Logo">
-					</span>
-				</a>
-			</div>
+    <!-- tab and smartphone nav  -->
+    <aside id="bt_sm_navbar1" class="bt_sm_navbar">
+        <div id="bt_btn1" class="bt_btn">
+            <div class="bt_line"></div>
+            <div class="bt_line"></div>
+            <div class="bt_line"></div>
+        </div>
+        <nav class="bt_sidebar">
+            <div class="bt_sm-logo-wrap">
+                <a href="index.php" id="bt_sm_logo">
+                    <span class="bt_logo-wrap">
+                        <img src="images/logo.png" alt="Builtech Logo">
+                    </span>
+                </a>
+            </div>
 
-			<ul>
-				<li><a href="#bt-about-section">About Us</a></li>
-				<li><a href="#bt-projects-section">Projects</a></li>
-				<li><a href="#bt-contact-section">Builtech edge</a></li>
-				<li><a href="#bt-contact-section">Home Owners</a></li>
-				<li><a href="#bt-contact-section">Contact Us</a></li>
-			</ul>
-		</nav>
+            <ul>
+                <li><a href="#bt-about-section">About Us</a></li>
+                <li><a href="#bt-projects-section">Projects</a></li>
+                <li><a href="#bt-contact-section">Builtech edge</a></li>
+                <li><a href="#bt-contact-section">Home Owners</a></li>
+                <li><a href="#bt-contact-section">Contact Us</a></li>
+            </ul>
+        </nav>
 
-		<p class="copyrights">All Rights Reserved | &copy; <script>
-				document.write(new Date().getFullYear());
-			</script> BUILTECH</p>
-	</aside>
-
-	<div id="bt-main">
-		<aside id="bt-hero" class="js-fullheight">
-			<div class="flexslider js-fullheight">
-				<ul class="slides">
-					<li class="ab_bt-hp-banner-1" style="background-image: url(images/projects/Rectangle.png );">
-						<div class="overlay"></div>
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-md-6 col-sm-12 col-xs-12 js-fullheight slider-text">
-									<!-- col-md-offset-3 col-md-push-3 -->
-									<div class="slider-text-inner">
-										<div class="ab_desc">
-											<div class="ab_desc-inner top">
-												<h2>About Us</h2>
-												<p class="ab_pr-intro">Being the pioneer of the apartment concept in Palakkad, Builtech stands tall on the foundation of its values - delivering quality, redefining customer lifestyles, cost effectiveness, time bound delivery ensuring customer
-													satisfaction and a proven track-record of post sales service.
-													It’s philosophy is founded on transparent dealings and maintaining ever lasting relationships with customers. Builtech’s credibility and good will has been meticulously developed over 35 years of integrity and highly satisfied customers.</p>
-												<a href="about.php" class="btn btn-primary btn-learn">Contact Us</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</li>
-				</ul>
-			</div>
-	</div>
-	<aside>
+        <p class="copyrights">All Rights Reserved | &copy; <script>
+                document.write(new Date().getFullYear());
+            </script> BUILTECH</p>
+    </aside>
 </div>
-
 <div id="bt-main">
-	<h1>Our Management</h1>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 col-sm-6">
-				<div id="wrapper">
-					<div id="outer">
-						<div class="itemwrapper desc">
-							<img class="image" src="images/image 8.jpg" alt="image" />
-						</div>
-						<div class="itemwrapper">
-							<img class="image" src="images/image 9.jpg" alt="image" />
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-md-6 col-sm-6">
-				<p class="para">
-					“Builtech has served client with happiness and satisfaction for many years. Today, Ananta epitomizes
-					our most treasured endeavor. A synergy of likeminded people, to create something that is very close to
-					the hearts of the senior members of our society. This project had been in the making for 10 years,<br>
-					requiring a visionary approach that understood the lifestyle and requirements of the elderly community.
-					With time, Ananta’s blueprint took shape and we began the journey to accomplish this project”<br>
-					Founder Mr. Rajan with Ar. Nithin Rajan.
-				</p>
-			</div>
-		</div>
-	</div>
+    <aside id="bt-hero" class="js-fullheight">
+        <div class="flexslider js-fullheight">
+            <ul class="slides">
+                <li class="ab_bt-hp-banner-1" style="background-image: url(images/projects/Rectangle.png );">
+                    <div class="overlay"></div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-12 col-xs-12 js-fullheight slider-text">
+                                <!-- col-md-offset-3 col-md-push-3 -->
+                                <div class="slider-text-inner">
+                                    <div class="ab_desc">
+                                        <div class="ab_desc-inner top">
+                                            <h2>About Us</h2>
+                                            <p class="ab_pr-intro">Being the pioneer of the apartment concept in Palakkad, Builtech stands tall on the foundation of its values - delivering quality, redefining customer lifestyles, cost effectiveness, time bound delivery ensuring customer
+                                                satisfaction and a proven track-record of post sales service.
+                                                It’s philosophy is founded on transparent dealings and maintaining ever lasting relationships with customers. Builtech’s credibility and good will has been meticulously developed over 35 years of integrity and highly satisfied customers.</p>
+                                            <a href="about.php" class="btn btn-primary btn-learn">Contact Us</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <aside>
 </div>
 
+</div>
+<!-- second page -->
+<div id="bt-main">
+    <div class="container">
+        <h1 class="title">Our Management</h1>
+
+        <div class="row ">
+            <div class="col-md-7">
+                <p class="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
+                    Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
+                    Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
+                    Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.
+                </p>
+
+            </div>
+            <div class="col-md-5">
+                <div class="section1">
+                    <img class="img1" src="images/image second.png" alt="image">
+                    <div class="board1">
+                        <h4 class="name">Mr.Rajan<br>
+                            <div class="designation">Director?</div>
+                        </h4>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row ">
+            <div class="col-md-5">
+                <div class="section2">
+                    <img class="img2" src="images/second image 2.png" alt="image">
+                    <div class="board2">
+                        <h4 class="name">Ar. Nithin Rajan
+                            <br>
+                            <div class="designation">Director?</div>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-7">
+                <p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
+                    Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
+                    Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
+                    Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include 'scripts.php'; ?>
