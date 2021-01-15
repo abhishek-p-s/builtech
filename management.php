074f5ee7585393b2fcbf9c5e00affef32317af63
abhishek <abhishekps6282@gmail.com>
2021-01-15 12:31:10 +0530

    mobile responsive

--- management.php
+++ management.php
@@ -89,55 +89,58 @@
                 </li>
             </ul>
         </div>
+        <aside>
 </div>
-<aside>
-    </div>
-    <!-- second page -->
-    <div id="bt-main">
-        <div class="container">
-            <h1>Our Management</h1>
 
-            <div class="row ">
-                <div class="col-md-7">
-                    <p class="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
-                        Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
-                        Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
-                        Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.
-                    </p>
+</div>
+<!-- second page -->
+<div id="bt-main">
+    <div class="container">
+        <h1 class="title">Our Management</h1>
 
-                </div>
-                <div class="col-md-5">
+        <div class="row ">
+            <div class="col-md-7">
+                <p class="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
+                    Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
+                    Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
+                    Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.
+                </p>
+
+            </div>
+            <div class="col-md-5">
+                <div class="section1">
                     <img class="img1" src="images/image second.png" alt="image">
                     <div class="board1">
                         <h4 class="name">Mr.Rajan<br>
                             <div class="designation">Director?</div>
                         </h4>
+
                     </div>
-                    <!-- <span class="tab-head1">Mr.Rajan<br>Directer?</span> -->
                 </div>
             </div>
+        </div>
 
-            <div class="row ">
-                <div class="col-md-5">
+        <div class="row ">
+            <div class="col-md-5">
+                <div class="section2">
                     <img class="img2" src="images/second image 2.jpg" alt="image">
                     <div class="board2">
                         <h4 class="name">Ar. Nithin Rajan
                             <br>
                             <div class="designation">Director?</div>
                         </h4>
-                        <i class="fa fa-linkedin" aria-hidden="true"></i>
                     </div>
-                    <!-- <span class="tab-head2">Ar. Nithin Rajan
-                Director?</span> -->
                 </div>
+            </div>
 
-                <div class="col-md-7">
-                    <p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
-                        Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
-                        Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
-                        Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
-                </div>
+
+            <div class="col-md-7">
+                <p class="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac turpis vel leo imperdiet sodales.
+                    Etiam sodales dui sem, luctus tempor tortor congue auctor. Quisque sit amet porta nisi. In hac habitasse platea dictumst.
+                    Praesent vitae ante justo. Nulla faucibus tellus eget dolor rutrum interdum.
+                    Pellentesque gravida nisl tortor, sit amet molestie eros euismod et. Ut scelerisque, sapien nec congue.</p>
             </div>
         </div>
     </div>
-    <?php include 'scripts.php'; ?>
+</div>
+<?php include 'scripts.php'; ?>
